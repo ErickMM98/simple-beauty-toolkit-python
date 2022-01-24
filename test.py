20567f7dc2f1be9e0b601308bcb9c865cb7f5104
Erick Muñiz Morales <erickmuniz@ciencias.unam.mx>
2022-01-24 02:51:42 -0600

    TODO: Add some font

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 import matplotlib as plt
-import
+import matplotlib.pyplot as plt
+from pprint import pprint
+import matplotlib as mpl
+plt.style.use('./simpletoolkit/styles/general.mplstyle')
+
+
+
+import numpy as np
+
+data = np.random.randn(50)
+plt.title('A title')
+plt.plot(data)
+plt.show()
+pprint( mpl.rcParams)
